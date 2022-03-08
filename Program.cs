ec35b6eb558056163c8b9e57d8738f1141ecdbd6
Aleksandr Stepanov <alex_stepanoff@inbox.ru>
2022-03-08 14:45:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
